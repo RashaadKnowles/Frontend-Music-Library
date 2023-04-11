a2fd6f4eb411b923f54897ef51a1d16cfabe45ed
unknown <rknowles12301@gmail.com>
2023-04-11 06:48:17 -0700

    Added in a submit button for user.

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,13 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios"
 const CreateSong = () => {
-    const newSong = {
-        title: "Vette Motors",
-        album: "The Last Slimeto",
-        artist: "Nba Youngboy",
-        genre: "hip-hop",
-        release_date: "2018-10-05",
-    }
+    
     async function createSong(song) {
         try {
             let response = await axios.post("http://127.0.0.1:5000/api/songs",song)
@@ -17,17 +11,45 @@ const CreateSong = () => {
         }
 
     }
+    const [title, setTtle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [album, setAlbum] = useState('');
+    const [genre, setGenre] = useState('');
+    const [release_date, setReleaseDate] = useState('');
+
+
+
+
 
     function handleSubmit (e){
+        let newSong = {
+        title: title,
+        album: album,
+        artist: artist,
+        genre: genre,
+        release_date: release_date,
+    }
         e.preventDefault()
         createSong(newSong)
     }
 
 
     return (
-        <div>
-            <button onClick={handleSubmit} >Send Song</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label>title</label>
+            <input type="text" value={title}  onChange={(event) => setTtle(event.target.value)}></input>
+            <lablel>album</lablel>
+            <input type="text" value={album}  onChange={(event) => setAlbum(event.target.value)}></input>
+            <label>artist</label>
+            <input type="text" value={artist}  onChange={(event) => setArtist(event.target.value)}></input>
+            <label>genre</label>
+            <input type="text" value={genre}  onChange={(event) => setGenre(event.target.value)}></input>
+            <label>release_date</label>
+            <input type="date" value={release_date}  onChange={(event) => setReleaseDate(event.target.value)}></input>
+            <button type="submit">Submit</button>
+        </form>
+            
+        
     );
 }
 
